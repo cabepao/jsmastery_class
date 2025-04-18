b772cb9cd507aff123b50747ae3207feedd3e7ef
cabepao <cabe.zz.2010@gmail.com>
2025-04-18 07:16:16 -0500

    update on searchbar

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,23 +1,28 @@
 import MovieCard from '@/components/MovieCard'
+import SearchBar from '@/components/SearchBar';
+import { icons } from '@/constants/icons';
 import { images } from '@/constants/images'
 import { fetchMovies } from '@/services/api';
 import useFetch from '@/services/useFetch';
-import { useRouter } from 'expo-router';
-import { View, Text, Image, FlatList } from 'react-native'
+import { useState } from 'react';
+import { View, Text, Image, FlatList, ActivityIndicator } from 'react-native'
 
-//hello git
-//im adding fom freateure-branch
-// added to sss
+
+
 const search = () => {
-  const router=useRouter();
+  const [searchQuery, setSearhQuery]=useState("");
+  
   const {
     data:movies
-    , loading:moviesLoagin
-    , error:moviesError}=useFetch(()=>fetchMovies({
-      query:''
-    }
+    , loading
+    , error
+    }=useFetch(()=>fetchMovies({
+      query:searchQuery
+    }),true)
 
-  ));
+    const handleSearch = (text: string) => {
+      setSearhQuery(text);
+    };
 
   return (
     <View className='flex-1 bg-primary '>
@@ -36,7 +41,45 @@ const search = () => {
         }
       }
       contentContainerStyle={{paddingBottom:100}}
-      
+      ListHeaderComponent={
+        <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search for a movie"
+                value={searchQuery}
+                onChangeText={handleSearch}
+              />
+            </View>
+
+            {loading && (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
+            )}
+
+            {error && (
+              <Text className="text-red-500 px-5 my-3">
+                Error: {error.message}
+              </Text>
+            )}
+
+            {!loading &&
+              !error &&
+              searchQuery.trim() &&
+              movies?.length! > 0 && (
+                <Text className="text-xl text-white font-bold">
+                  Search Results for{" "}
+                  <Text className="text-accent">{searchQuery}</Text>
+                </Text>
+              )}
+          </>
+      }
       />
 
     </View>
